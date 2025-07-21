8106392575513226ddf2f8ff2b1866386b41f39a
Celal Karakoç <c.karakoc@live.nl>
2025-07-21 08:30:20 +0200

    Added bootstrap for a single project ng add _ --project _

--- projects/app2/src/app/app.ts
+++ projects/app2/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgbDatepicker } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgbDatepicker],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
